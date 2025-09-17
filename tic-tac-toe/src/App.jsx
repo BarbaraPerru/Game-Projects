4c7dc23cbff047f920730cb2b098919a02ee0758
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-09-17 17:20:14 +0200

    lift up State and Log component created

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,17 +1,26 @@
 import Player from "./components/Player"  
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log"  
+import { useState } from "react"
+
 function App() {
-  
+  const [gameTurn, setGameTurn] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
+  function handleSelectCell() {
+    setActivePlayer((curActivePlayer)=> curActivePlayer=== 'X' ? 'O' : 'X');
+    setGameTurn((curGameTurn) => [...curGameTurn, 'X']);
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectCell={handleSelectCell} activePlayerSymbol={activePlayer} />
       </div>
+      <Log />
     </main>
   )
 }
--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 const initialGameBoard = [[null,null,null],[null,null,null],[null,null,null]];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectCell, activePlayerSymbol}) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectCell(rowIndex, cellIndex) {
+        setGameBoard((preGameBoard) => {
+            const updatedGameBoard = [...preGameBoard.map(innerArray=>[...innerArray])];
+            updatedGameBoard[rowIndex][cellIndex] = activePlayerSymbol;
+            return updatedGameBoard;
+        });
+        onSelectCell(rowIndex, cellIndex);
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex)=><li key={rowIndex}>
+            {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, cellIndex)=><li key={cellIndex}><button>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, cellIndex)=><li key={cellIndex}><button onClick={() => handleSelectCell(rowIndex, cellIndex)}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
