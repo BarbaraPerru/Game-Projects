faf33b7ea358e0eb69e0c7cdf3c47e0a6b381d23
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-09-18 16:54:43 +0200

    deriving computed values from other computed values (i.e. winner)

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,8 +2,8 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"  
 import { useState } from "react"
-
-
+import { WINNING_COMBINATIONS } from "./winning-combination.js"  
+const initialGameBoard = [[null,null,null],[null,null,null],[null,null,null]];
 function deriveActivePlayer(gameTurn) {
   let currPlayer = 'X';
       if(gameTurn.length > 0 && gameTurn[0].player === 'X') {
@@ -14,6 +14,24 @@ function deriveActivePlayer(gameTurn) {
 function App() {
   const [gameTurn, setGameTurn] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurn);
+  
+  let gameBoard = initialGameBoard;
+    for(const turn of gameTurn) {
+        const {square, player} = turn;
+        const {row, cell} = square;
+        gameBoard[row][cell] = player;
+    }
+
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].cell];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].cell];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].cell];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
   function handleSelectCell(rowIndex, cellIndex) {
     
     setGameTurn((prevTurn) =>{
@@ -31,7 +49,8 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectCell={handleSelectCell} activePlayerSymbol={activePlayer}  turns={gameTurn}/>
+        {winner && <p>{winner} wins!</p>}
+        <GameBoard onSelectCell={handleSelectCell} activePlayerSymbol={activePlayer}  board={gameBoard}/>
       </div>
       <Log turns={gameTurn} />
     </main>
--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,28 +1,11 @@
-const initialGameBoard = [[null,null,null],[null,null,null],[null,null,null]];
 
-export default function GameBoard({onSelectCell, turns}) {
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    // function handleSelectCell(rowIndex, cellIndex) {
-    //     setGameBoard((preGameBoard) => {
-    //         const updatedGameBoard = [...preGameBoard.map(innerArray=>[...innerArray])];
-    //         updatedGameBoard[rowIndex][cellIndex] = activePlayerSymbol;
-    //         return updatedGameBoard;
-    //     });
-    //     onSelectCell(rowIndex, cellIndex);
-    // }
-    let gameBoard = initialGameBoard;
-    for(const turn of turns) {
-        const {square, player} = turn;
-        const {row, cell} = square;
-        gameBoard[row][cell] = player;
-    }
-   
+export default function GameBoard({onSelectCell, board}) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
+            {board.map((row, rowIndex)=><li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, cellIndex)=><li key={cellIndex}><button onClick={()=> onSelectCell(rowIndex, cellIndex)}>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, cellIndex)=><li key={cellIndex}><button onClick={()=> onSelectCell(rowIndex, cellIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
