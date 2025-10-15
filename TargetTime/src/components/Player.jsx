3c79403ce2bbcd7f61fbe1382315deecd65c9daf
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-10-15 10:59:12 +0200

    User input managed trought useState

--- TargetTime/src/components/Player.jsx
+++ TargetTime/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submittedPlayerName, setSubmittedPlayerName] = useState('');
+  function handleSetPlayerName(event) {
+    setEnteredPlayerName(event.target.value);
+  }
+  function handleClick(){
+    setSubmittedPlayerName(true);
+   }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submittedPlayerName ? enteredPlayerName : 'Unknown'} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleSetPlayerName} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
