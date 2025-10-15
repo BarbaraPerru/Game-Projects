9cfef6e0571dccb7b94d0ab1e0a08c42cc7ba101
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-10-15 15:33:32 +0200

    starting button problem solved with useRef

--- TargetTime/src/components/TimerChallenge.jsx
+++ TargetTime/src/components/TimerChallenge.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react';
-let timer;
+import { useState, useRef } from 'react';
+
 export default function TimerChallenge({title, targetTime}) {
+    const timer=useRef();
     const [timeStarted, setTimeStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
     function handleStartGame(){
-        timer= setTimeout(() => {
+        timer.current= setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
         setTimeStarted(true);
     }
     function handleStopGame(){
-        clearTimeout(timer);
+        clearTimeout(timer.current);
     }
     return (
         <section className="challenge">
             <h2>{title}</h2>
             {timerExpired && <p className="challenge-time">Time's ran out</p>}
             <p className="challenge-time">
-                {targetTime} second'{targetTime > 1 ? 's' : ''}
+                {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <button onClick={timeStarted ? handleStopGame : handleStartGame}>{timeStarted ? 'Stop' : 'Start'}</button>
+            
             <p className={timeStarted ? 'active' : undefined}>{timeStarted ? 'Started' : 'Not started'}</p>
         </section>
     );
